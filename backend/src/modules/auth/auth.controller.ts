786a03d5cb4c541ec13cf67972771564d2006e59
ants-1 <anthonysalisbury98@outlook.com>
2026-06-01 23:23:11 +0100

    feat: add Zod validation schema for auth

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,24 +1,43 @@
 import { Request, Response, NextFunction } from "express";
 import authService from "../auth/auth.service";
+import { registerUserSchema, loginUserSchema } from "./auth.schema";
+import * as z from "zod";
 
 const register = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const userData = req.body;
+    registerUserSchema.parse(userData);
+
     const user = await authService.register(req.body);
     res.status(201).json(user);
   } catch (error: any) {
-    res.status(400).json({ error: error.message });
+    if (error instanceof z.ZodError) {
+      const formattedError = z.prettifyError(error);
+      return res.status(400).json({ error: formattedError });
+    }
+
+    res.status(500).json({ error: error.message });
   }
 };
 
 const login = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const userData = req.body;
+    loginUserSchema.parse(userData);
+
     const user = await authService.login(req.body);
 
     if (!user.token) {
       res.status(401).json({ message: "Invalid crendentials" });
     }
+    
     res.status(200).json(user);
   } catch (error: any) {
+    if (error instanceof z.ZodError) {
+      const formattedError = z.prettifyError(error);
+      return res.status(400).json({ error: formattedError });
+    }
+
     res.status(500).json({ error: error.message });
   }
 };
