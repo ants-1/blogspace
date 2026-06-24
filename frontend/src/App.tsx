7947c69f08d7414a5615bd892d0ba2c69f1b70d6
ants-1 <anthonysalisbury98@outlook.com>
2026-06-24 13:14:36 +0100

    chore: setup Chakra UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import { HStack, Button } from "@chakra-ui/react";
+
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+    <HStack>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </HStack>
+  );
 }
 
 export default App;
